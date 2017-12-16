03855929855243264d58898adec53d46c13216a6
Kevin Alberts <kevin@kevinalberts.nl>
2017-12-16 16:34:03 +0100

    Added day 16

---
+++ day16.py
@@ -0,0 +1,52 @@
+programs = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 
+            'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p']
+
+def spin(programs, num):
+    e = programs[-num:]
+    b = programs[:-num]
+    return e + b
+
+def exchange(programs, a, b):
+    programs[b], programs[a] = programs[a], programs[b]
+    return programs
+
+def partner(programs, a, b):
+    ia = programs.index(a)
+    ib = programs.index(b)
+    return exchange(programs, ia, ib)
+
+with open('day16.in', 'r') as f:
+    inp = f.readlines()[0].strip().split(',')
+
+def step(programs):
+    for i in inp:
+        if i[0] == "s":
+            programs = spin(programs, int(i[1:]))
+        elif i[0] == "x":
+            a, b = i[1:].split("/")
+            programs = exchange(programs, int(a), int(b))
+        elif i[0] == "p":
+            a, b = i[1:].split("/")
+            programs = partner(programs, a, b)
+    return programs
+
+print("".join(programs))
+
+start_programs = programs.copy()
+repetition = False
+rcount = 0
+
+while not repetition:
+    programs = step(programs)
+    rcount += 1
+    if programs == start_programs:
+        repetition = True
+
+print("Repetition found after {} cycles".format(rcount))
+
+end_step = 1000000000 % rcount
+for x in range(end_step):
+    programs = step(programs)
+
+print("Stopped on cycle {}".format(end_step))
+print("".join(programs))
